6a26c0c265e65f2015de99b82bf68db0f0f872f5
maksimualdTim <timofeev2001maksim@gmail.com>
2025-01-15 17:02:41 +0500

    add connection and delete

---
+++ app/Http/Mail/SendPassword.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendPassword extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(
+        private string $mail,
+        private string $password,
+        private string $login,
+    )
+    {
+        //
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(env('MAIL_FROM_ADDRESS')),
+            subject: 'Login Password',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'email.sendPassword',
+            with: [
+                'login' => $this->login,
+                'password' => $this->password,
+                'mail' => $this->mail,
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
--- app/Services/SettingsService.php
+++ app/Services/SettingsService.php
@@ -1,6 +1,10 @@
 <?php
 namespace App\Services;
 
+use App\Models\Connection;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
+
 class SettingsService {
     const STATUS_PASSWORD_NEED = 1;
     const STATUS_VERIFYED = 2;
@@ -9,6 +13,12 @@ class SettingsService {
     public function sendTelegramVerificationCode(string $phone): void 
     {
         $MadelineProto = $this->createMadelineProto($phone);
+        $user = auth()->user()->load('account');
+
+        $user->account->connections()->create([
+            'phone' => $phone
+        ]);
+        
         $MadelineProto->phoneLogin($phone);
     }
 
@@ -36,13 +46,33 @@ class SettingsService {
         return self::STATUS_VERIFYED;
     }
 
-    private function createMadelineProto(string $phone): \danog\MadelineProto\API {
-        $settings = (new \danog\MadelineProto\Settings\AppInfo)
-        ->setApiId(intval(env("TELEGRAM_API_ID")))
-        ->setApiHash(env('TELEGRAM_API_HASH'));
+    public function deleteConnection(string $phone) {
+        $storagePath = $this->getStoragePath($phone);
+        File::deleteDirectory($storagePath);
+        DB::table('connections')->where(['phone' => $phone, 'account_id' => auth()->user()->account->id])->delete();
+    }
 
-        $storagePath = storage_path("app/telegram/{$phone}.madeline");
+    private function createMadelineProto(string $phone): \danog\MadelineProto\API {
+        $settings = new \danog\MadelineProto\Settings;
+        // $settings->setDb(
+        //     (new \danog\MadelineProto\Settings\Database\Postgres)
+        //     ->setDatabase(env("DB_DATABASE"))
+        //     ->setUsername(env("DB_USERNAME"))
+        //     ->setPassword(env("DB_PASSWORD"))
+        // );
+
+        $settings->setAppInfo(
+            (new \danog\MadelineProto\Settings\AppInfo)
+            ->setApiId(intval(env("TELEGRAM_API_ID")))
+            ->setApiHash(env('TELEGRAM_API_HASH'))
+        );
+
+        $storagePath = $this->getStoragePath($phone);
 
         return new \danog\MadelineProto\API($storagePath, $settings);
+    }
+
+    private function getStoragePath(string $phone): string {
+        return storage_path("app/telegram/{$phone}.madeline");
     }
 }
