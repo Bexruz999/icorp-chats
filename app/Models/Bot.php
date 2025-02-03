7b01affd04d59ad5baa05753db2b41f8fa0932fc
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-02-03 12:48:55 +0500

    bot add error

--- app/Models/Bot.php
+++ app/Models/Bot.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Bot extends Model
 {
+
+    protected $fillable = [
+        'name',
+        'token',
+        'slug',
+        'account_id'
+    ];
     public function shops(): HasOne {
         return $this->hasOne(Shop::class);
     }
