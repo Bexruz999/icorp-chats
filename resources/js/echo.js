4c1e7636d3a63ba078f5b5c8e46c1d3d8484c427
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-04-25 11:35:18 +0500

    wss

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -7,7 +7,7 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     host: `https://commni-amochat.icorp.uz:${import.meta.env.VITE_REVERB_PORT}`,
-    wsHost: 'https://commni-amochat.icorp.uz',
+    wsHost: 'commni-amochat.icorp.uz',
     wsPort: import.meta.env.VITE_REVERB_PORT,
     wssPort: import.meta.env.VITE_REVERB_PORT,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
