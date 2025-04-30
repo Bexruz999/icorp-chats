b7e8fbebabeb5cc7d5337f1de009b243eb8887f6
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-04-30 15:07:35 +0500

    incoming message to amo crm chat

--- app/Listeners/TelegramIncomingMessage.php
+++ app/Listeners/TelegramIncomingMessage.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 
 use App\Events\SendAmoCrmMessage;
 use App\Events\TelegramMessage;
+use App\Services\AmoChatService;
 use danog\MadelineProto\EventHandler\Attributes\Handler;
 use danog\MadelineProto\EventHandler\Media\Audio;
 use danog\MadelineProto\EventHandler\Media\Document;
@@ -13,6 +14,7 @@ use danog\MadelineProto\EventHandler\Media\Video;
 use danog\MadelineProto\EventHandler\Media\Voice;
 use danog\MadelineProto\EventHandler\Message;
 use danog\MadelineProto\SimpleEventHandler;
+use Str;
 
 class TelegramIncomingMessage extends SimpleEventHandler
 {
@@ -20,6 +22,11 @@ class TelegramIncomingMessage extends SimpleEventHandler
     #[Handler]
     public function handleMessage(Message $message): void
     {
+        (new AmoChatService())->sendMessage(
+            peer_id: rand(100, 1000),
+            msg_id: rand(100, 1000),
+            msg: Str::random(20),
+        );
         TelegramMessage::dispatch($message);
     }
 }
