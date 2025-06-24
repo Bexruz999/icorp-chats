8471992c9074b288bb96987c4a1c38d66e70a7d2
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-06-24 20:22:44 +0500

    {ht?}

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Api\Webhook\AmoChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-Log::debug('headers ' . json_encode(Request::headers()->all()));
+//Log::debug('headers ' . json_encode(Request::headers()->all()));
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
