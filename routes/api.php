9c47749a3d4f9696825545126a09401d15cc52f4
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-06-24 20:21:31 +0500

    {ht?}

--- routes/api.php
+++ routes/api.php
@@ -8,4 +8,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/amochat/{scope_id}/{ht?}', [AmoChatController::class, 'handle']);
+Route::post('/amochat/{scope_id}', [AmoChatController::class, 'handle']);
