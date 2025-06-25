90a98a819662f46cf93d5356468fc5c1e316d0b7
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-06-25 18:22:42 +0500

    {ht?}

--- app/Http/Controllers/Api/Webhook/AmoChatController.php
+++ app/Http/Controllers/Api/Webhook/AmoChatController.php
@@ -15,6 +15,7 @@ class AmoChatController extends Controller
 {
     public function handle(Request $request, TelegramService $telegram)
     {
+        return abort(500);
         if (empty($request->header('X-Signature'))) {
             return response()->json(['message' => 'Invalid signature'], 403);
         }
