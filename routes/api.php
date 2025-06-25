7a6416aa2121b5d5be222a07fc9b7a4ebd8bc931
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-06-25 17:53:12 +0500

    {ht?}

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Api\Webhook\AmoChatController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 Log::debug('test' . json_encode([request()->all(), request()->headers->all()]));
-Route::post('/amochat/{scope_id}', [AmoChatController::class, 'handle']);
+Route::post('/amochat/{scope_id}', [AmoChatController::class, 'handle'])
+    ->where('scope_id', '.*');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
