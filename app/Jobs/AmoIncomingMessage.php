a4e5cb87589185bddc0d0e6f7bce3222b48ad21f
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-06-25 21:47:46 +0500

    telegram phone and Cache duplicate

--- app/Jobs/AmoIncomingMessage.php
+++ app/Jobs/AmoIncomingMessage.php
@@ -38,8 +38,10 @@ class AmoIncomingMessage implements ShouldQueue
      */
     public function handle(): void
     {
-        $amo = new AmoChatService($this->connect);
-        Log::debug('This: ' . json_encode([$this->contact, $this->message, $this->connect]));
-        $amo->sendMessage(contact: $this->contact, msg: $this->message);
+        if (!Cache::has("amocrm_{$this->connect['id']}-{$this->message['id']}")) {
+            $amo = new AmoChatService($this->connect);
+            Log::debug('This: ' . json_encode([$this->contact, $this->message, $this->connect]));
+            $amo->sendMessage(contact: $this->contact, msg: $this->message);
+        }
     }
 }
