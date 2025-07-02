b96066e3785708c52d518c67be39b5fd10e1fe38
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-07-02 15:17:15 +0500

    Refactor Create component and EmployeesController to enhance user experience and connection handling

--- app/Services/AmoApiService.php
+++ app/Services/AmoApiService.php
@@ -26,6 +26,7 @@ class AmoApiService
     public function __construct(User $user)
     {
         $this->user = $user ?? auth()->user();
+        Log::debug('this user: ' . var_export($this->user, true));
         $this->provider = new AmoCRM([
             'clientId' => config('amo.integration_id'),
             'clientSecret' => config('amo.integration_secret_key'),
